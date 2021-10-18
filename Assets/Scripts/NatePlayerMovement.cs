247330273138b6e85aeebd041085f19ffce632f4
NW360-DDR <90855384+NW360-DDR@users.noreply.github.com>
2021-10-18 19:31:14 -0400

    Movement Added

--- Assets/Scripts/NatePlayerMovement.cs
+++ Assets/Scripts/NatePlayerMovement.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class NatePlayerMovement : MonoBehaviour
 {
-    public string leftControl, rightControl, upControl, downControl, shootControl;
+    public string leftControl, rightControl, jumpControl, crouchControl, shootControl;
+    public string[] defaultControls = new string[5];
     public float playerSpeed;
+    public float jumpPower;
 
+    public bool grounded = true;
+    public Vector3 defaultSize;
+    public Vector3 crouchSize;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +21,37 @@ public class NatePlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Time to start checking for movement the hard way due to our convoluted controls. First the X axis.
+        if (Input.GetKey(leftControl))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * playerSpeed);
+        }
+        else if (Input.GetKey(rightControl))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * playerSpeed);
+        }
+        //Now for jumping.
+        if (Input.GetKeyDown(jumpControl) && grounded)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            grounded = false;
+        }
+        // Aaand for crouching, we need to be grounded
+        if (Input.GetKeyDown(crouchControl) && grounded)
+        {
+            transform.localScale = new Vector3(1, 0.6f, 1);
+        }
+        if (!grounded || Input.GetKeyUp(crouchControl))
+        {
+            transform.localScale = Vector3.one;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
     }
 }
