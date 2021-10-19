717062bb5b929d2d9d70fe7f3bf8ec94e66bd15f
NW360-DDR <90855384+NW360-DDR@users.noreply.github.com>
2021-10-18 20:10:33 -0400

    Movement Randomizer Added

--- Assets/Scripts/NatePlayerMovement.cs
+++ Assets/Scripts/NatePlayerMovement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class NatePlayerMovement : MonoBehaviour
 {
-    public string leftControl, rightControl, jumpControl, crouchControl, shootControl;
-    public string[] defaultControls = new string[5];
+    private string leftControl, rightControl, jumpControl, crouchControl, shootControl;
+    public string[] controls = new string[5]; // Left, Right, Jump, Crouch, Shoot
     public float playerSpeed;
     public float jumpPower;
 
@@ -22,28 +22,35 @@ public class NatePlayerMovement : MonoBehaviour
     void Update()
     {
         // Time to start checking for movement the hard way due to our convoluted controls. First the X axis.
-        if (Input.GetKey(leftControl))
+        if (Input.GetKey(controls[0]))
         {
             transform.Translate(Vector2.left * Time.deltaTime * playerSpeed);
         }
-        else if (Input.GetKey(rightControl))
+        else if (Input.GetKey(controls[1]))
         {
             transform.Translate(Vector2.right * Time.deltaTime * playerSpeed);
         }
         //Now for jumping.
-        if (Input.GetKeyDown(jumpControl) && grounded)
+        if (Input.GetKeyDown(controls[2]) && grounded)
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             grounded = false;
         }
         // Aaand for crouching, we need to be grounded
-        if (Input.GetKeyDown(crouchControl) && grounded)
+        if (Input.GetKeyDown(controls[3]) && grounded)
         {
             transform.localScale = new Vector3(1, 0.6f, 1);
         }
-        if (!grounded || Input.GetKeyUp(crouchControl))
+        if (!grounded || Input.GetKeyUp(controls[3]))
         {
             transform.localScale = Vector3.one;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Randomizing controls!");
+            RandomizeControls(controls);
         }
     }
 
@@ -53,5 +60,20 @@ public class NatePlayerMovement : MonoBehaviour
         {
             grounded = true;
         }
+    }
+
+
+    public void RandomizeControls(string[] controls) // Changes all the buttons at random.
+    {
+        // Knuth shuffle algorithm, many thanks to StackOverflow for suggesting it, and Wikipedia for supplying a generic codebase.
+        for (int t=0; t < controls.Length; t++)
+        {
+            string temp = controls[t];
+            int r = Random.Range(t, controls.Length);
+            controls[t] = controls[r];
+            controls[r] = temp;
+        }
+
+        
     }
 }
