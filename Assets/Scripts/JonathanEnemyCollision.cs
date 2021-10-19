6270c53dc2b0a47e795e762b42e42e12db776bc0
JA-FSU <78230541+JA-FSU@users.noreply.github.com>
2021-10-18 20:23:23 -0400

    BulletShooting script

--- Assets/Scripts/JonathanEnemyCollision.cs
+++ Assets/Scripts/JonathanEnemyCollision.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class JonathanEnemyCollision : MonoBehaviour
 {
+    private GameManager gameManager;
+    Rigidbody2D enemyRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        enemyRb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Projectile"))
+        {
+
+        }
     }
 }
