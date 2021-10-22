828e73dd7efb07953448065b1180f53c97301a18
NW360-DDR <90855384+NW360-DDR@users.noreply.github.com>
2021-10-21 22:41:28 -0400

    Update JonathanEnemyCollision.cs

--- Assets/Scripts/JonathanEnemyCollision.cs
+++ Assets/Scripts/JonathanEnemyCollision.cs
@@ -20,7 +20,7 @@ public class JonathanEnemyCollision : MonoBehaviour
         
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Projectile"))
         {
